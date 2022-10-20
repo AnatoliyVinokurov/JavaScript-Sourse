e3ea32c816fbbbe9b8969098c651092a35e0f1f3
AnatoliyVinokurov <anatolij.isit@gmail.com>
2022-10-20 22:47:41 +0300

    37

---
+++ 37/h.js
@@ -0,0 +1,12 @@
+let res = sumDigits(1234);
+console.log(`res = ${res}`);
+res = luckyNumber(123871);
+console.log(res ? 'lucky' : 'unlucky');
+
+function sumDigits(num){
+
+}
+
+function luckyNumber(num){
+    
+}
