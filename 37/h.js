2ed6a746e743ea0d87fea98fcf9c7f545d6b90ea
AnatoliyVinokurov <anatolij.isit@gmail.com>
2022-10-21 18:47:17 +0300

    37 add coments

--- 37/h.js
+++ 37/h.js
@@ -1,12 +1,55 @@
 let res = sumDigits(1234);
 console.log(`res = ${res}`);
-res = luckyNumber(123871);
+res = luckyNumber2(1238721);
 console.log(res ? 'lucky' : 'unlucky');
 
 function sumDigits(num){
-
+    let sum = 0;
+    while(num !== 0){
+        sum += num %10;
+        num = (num - num % 10) / 10;
+    }
+    return sum;
 }
 
 function luckyNumber(num){
-    
+    let sum1 = 0;
+    let sum2 = 0;
+    let pos = 1;
+    while (num !== 0){
+        if (pos % 2 === 0){
+            sum1 += num % 10;
+        }else{
+            sum2 += num % 10;
+        }
+        num = (num - num % 10) / 10;
+        pos++;
+    }
+    // if (sum1 === sum2){
+    //     return true;
+    // }else{
+    //     return false;
+    // }
+    return sum1 === sum2;
+}
+
+function luckyNumber1(num){
+    let sum1 = 0;
+    let sum2 = 0;
+    while (num !== 0){
+       sum1 += num % 10;
+       num = (num - num % 10) / 10;
+       sum2 += num % 10;
+       num = (num - num % 10) / 10;
+    }
+    return sum1 === sum2;
+}
+
+function luckyNumber2(num){
+    let sum = 0;
+    while (num !== 0){
+        sum = num % 10 - sum;
+        num = (num - num % 10)/10;
+    }
+    return sum === 0;
 }
